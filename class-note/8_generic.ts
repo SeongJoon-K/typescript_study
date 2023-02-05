fb946d4c5a0e2d739054ac940cf35cd02e7d4e37
SEONGJOON KIM <jobcho6320@naver.com>
2023-02-05 22:48:05 +0900

    [제네릭]

--- class-note/8_generic.ts
+++ class-note/8_generic.ts
@@ -18,10 +18,8 @@
  * 함수를 사용할 때 사용하는 시점에 타입을 넘기는 것이다.
  */
 
-function logText(text:string|number) { //여러가지 타입으로 받을 수 있는 게 유니온 타입이다.
+function logText<T>(text:<T>):T { //여러가지 타입으로 받을 수 있는 게 유니온 타입이다.
     console.log(text);
-    // text.
-    // 유니온 타입을 사용해 메소드에 접근하면 string, number모두 겹치는 메소드 미리보기만 제공함
     return text;
 }
 
@@ -34,3 +32,8 @@ a.split('')
  * 이런 문제가 발생하는 이유는 input에 대한 것은 해결이 되었으나
  * 반환 값에 대한 해결이 되지 않았을 때 발생하는 것이다.
  */
+
+const str = logText<string>('abc');
+str.split('');
+
+const login = logText<boolean>(true);
